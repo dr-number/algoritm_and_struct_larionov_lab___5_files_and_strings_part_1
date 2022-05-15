9d424c05e206e37e0446b56ae786eecbb000e0db
Lar97 <dr.number@yandex.ru>
2022-05-15 14:33:05 +0700

    correct task Part_1_Task_6_2

--- larionov_lab___5_files_and_strings_part_1/Program.cs
+++ larionov_lab___5_files_and_strings_part_1/Program.cs
@@ -470,15 +470,73 @@ namespace larionov_lab___5_files_and_strings_part1
 
     public class Part_1_Task_6_2
     {
+        const string DEFAULT_SYMBOL = "М";
+        private void PrintSplint(string str, string symbol, bool isReg)
+        {
+            string[] array = str.Split(" ");
+
+            int count = 0;
+            string result = "";
+
+            bool isRegistr, isNoRegistr;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nДанные после обработки: \n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            foreach (var item in array)
+            {
+                isNoRegistr = !isReg && item.ToLower().Contains(symbol);
+                isRegistr = isReg && item.Contains(symbol);
+
+                if (isRegistr || isNoRegistr)
+                {
+                    result += item + " ";
+                    ++count;
+                }
+            }
+
+            MyPrint myPrint = new MyPrint();
+            myPrint.printString("\nКоличество слов:", count.ToString());
+        }
+
         public void init()
         {
             Console.WriteLine(TasksInfo.PART_1_TASK_6_2);
 
+            MyQuestion myQuestion = new MyQuestion();
+            bool isReg = myQuestion.isQuestion("Учитывать регистр? [y/n]: ");
+
             MyStrings myStrings = new MyStrings();
             string str = myStrings.getFirstString("Введите строку: ", MyFiles.FILE_PART_1_TASK_6_2, ".");
 
+            string findSymbol = DEFAULT_SYMBOL;
+
             MyPrint myPrint = new MyPrint();
             myPrint.printString("\n" + MyPrint.INITIAL_DATA, str, "\n");
+            myPrint.printString("Найти слова содержашие букву", $"\"{findSymbol}\"", "\n");
+
+            if(isReg)
+                myPrint.printString("Выполнить поиск", "с учетом регистра", "\n");
+            else
+                myPrint.printString("Выполнить поиск", "без учета регистра", "\n");
+
+            if (myQuestion.isQuestion("Выпонить поиск ориентируясь на другие символ(ы)? [y/n]: "))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Введите символ(ы): ");
+                Console.ResetColor();
+
+                string tmp = Console.ReadLine();
+
+                if (tmp != "" && tmp != " ")
+                    findSymbol = tmp;
+
+                myPrint.printString("Найти слова содержашие букву", $"\"{findSymbol}\"", "\n");
+            }
+
+            PrintSplint(str, findSymbol, isReg);
         }
     }
 
